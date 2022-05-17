cec0c42905f6ce89f638758b45492958f9ecfef6
lipaoos <fillipe_oliveira@outlook.com.br>
2022-05-17 10:05:36 -0300

    criacao do viewmodel e validacoes simples

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 using Blog.Data;
 using Blog.Models;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,51 +13,92 @@ namespace Blog.Controllers
         [HttpGet("v1/categories")]
         public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
         {
-            var categories = await context.Categories!.AsNoTracking().ToListAsync();
-            return Ok(categories);
+            try
+            {
+                var categories = await context.Categories!.AsNoTracking().ToListAsync();
+                return Ok(categories);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
 
         }
 
         [HttpGet("v1/categories/{id:int}")]
         public async Task<IActionResult> GetByIdAsync([FromServices] BlogDataContext context, [FromRoute] int id)
         {
-            var category = await context.Categories!.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            try
+            {
+                var category = await context.Categories!.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
 
-            if (category == null) return NotFound();
-            return Ok(category);
+                if (category == null) return NotFound();
+                return Ok(category);
+            }
+            catch (System.Exception)
+            {
+
+                return BadRequest();
+            }
 
         }
 
         [HttpPost("v1/categories")]
-        public async Task<IActionResult> PostAsync([FromServices] BlogDataContext context, [FromBody] Category category)
+        public async Task<IActionResult> PostAsync([FromServices] BlogDataContext context, [FromBody] EditorCategoryViewModel model)
         {
-            await context.Categories!.AddAsync(category);
-            await context.SaveChangesAsync();
+            try
+            {
+                var category = new Category()
+                {
+                    Name = model.Name,
+                    Slug = model.Slug
+                };
 
-            return Created($"v1/categories/{category.Id}", category);
+                await context.Categories!.AddAsync(category);
+                await context.SaveChangesAsync();
+
+                return Created($"v1/categories/{category.Id}", category);
+            }
+            catch (System.Exception)
+            {
+
+                return BadRequest();
+            }
         }
 
         [HttpPut("v1/categories/{id:int}")]
-        public async Task<IActionResult> PutAsync([FromServices] BlogDataContext context, [FromRoute] int id, [FromBody] Category model)
+        public async Task<IActionResult> PutAsync([FromServices] BlogDataContext context, [FromRoute] int id, [FromBody] EditorCategoryViewModel model)
         {
-            var category = await context.Categories!.FirstOrDefaultAsync(x => x.Id == id);
-            if (category == null) return NotFound();
-            category = model;
-            context.Update(category);
-            await context.SaveChangesAsync();
-            return Ok();
+            
+                var category = await context.Categories!.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null) return NotFound();
+                category.Name = model.Name;
+                category.Slug = model.Slug;
+                context.Categories!.Update(category);
+                await context.SaveChangesAsync();
+                return Ok();
+            
+            
 
         }
 
         [HttpDelete("v1/categories/{id:int}")]
         public async Task<IActionResult> DeleteAsync([FromServices] BlogDataContext context, [FromRoute] int id)
         {
-            var category = await context.Categories!.FirstOrDefaultAsync(x => x.Id == id);
-            if (category == null) return NotFound();
-            context.Categories!.Remove(category);
-            await context.SaveChangesAsync();
+            try
+            {
+                var category = await context.Categories!.FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null) return NotFound();
+                context.Categories!.Remove(category);
+                await context.SaveChangesAsync();
 
-            return Ok();
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+
+                return BadRequest();
+            }
 
 
         }
