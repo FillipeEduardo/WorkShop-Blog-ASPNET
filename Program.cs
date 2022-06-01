f42bc4bf2f0675d219f14682494ecc0f021b0e0b
lipaoos <fillipe_oliveira@outlook.com.br>
2022-06-01 10:20:50 -0300

    criado a classe tokenservice e o metodo generatetoken

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 using Blog.Data;
+using Blog.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().ConfigureApiBehaviorOptions(o => o.SuppressModelStateInvalidFilter = true);
 builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddTransient<TokenService>();
+
+
 
 var app = builder.Build();
 
